dd3b602679a7d2dddd647aff5afbfd5c5e8501d7
xianhongtao <xianhongtao2022@outlook.com>
2025-03-17 11:08:44 +0800

    删除 Test.java 文件，新增 shippingPriceCalculator.java 计算运费的功能

---
+++ shippingPriceCalculator.java
@@ -0,0 +1,31 @@
+public class shippingPriceCalculator {
+    public static void main(String[] args) {
+        double weight = 0;
+        double price = 0;
+        double discount = 0;
+        double total = 0;
+        System.out.println("请输入货物的重量：");
+        weight = Double.parseDouble(System.console().readLine());
+        System.out.println("请输入货物的单价：");
+        price = Double.parseDouble(System.console().readLine());
+        switch ((int)(weight/250)){
+            case 0:
+                discount = 0;
+                break;
+            case 1:
+                discount = 0.02;
+                break;
+            case 2:
+                discount = 0.05;
+                break;
+            case 3:
+                discount = 0.08;
+                break;
+            default:
+                discount = 0.1;
+        }
+        System.out.println("折扣为：" + discount);
+        total = price * weight * (1 - discount);
+        System.out.println("总价为：" + total);
+    }
+}
