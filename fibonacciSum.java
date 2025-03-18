544fb573981456207fb2ef024d8ff35c3bc95663
xianhongtao <xianhongtao2022@outlook.com>
2025-03-18 09:18:32 +0800

    新增 fibonacciSum.java 文件，实现斐波那契数列及其和的计算功能

---
+++ fibonacciSum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class fibonacciSum {
+    public static void main(String[] args) {
+        int Var1 = 0;
+        int Var2 = 1;
+        int totalSum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入数列的项数：");
+        int num = sc.nextInt();
+        for (int i = 1; i <= num; i++) {
+            System.out.print(Var1 + " ");
+            int sum = Var1 + Var2;
+            Var1 = Var2;
+            Var2 = sum;
+            totalSum += Var1;
+        }
+        System.out.println("\n" + "数列的和为：" + totalSum);
+    }
+}
