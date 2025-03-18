1f1558a3fd5bac98c4e06357c5bd9c574bcf8d48
xianhongtao <xianhongtao2022@outlook.com>
2025-03-18 09:13:21 +0800

    优化 fibonacci.java 文件，简化斐波那契数列计算逻辑

--- fibonacci.java
+++ fibonacci.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 public class fibonacci {
     public static void main(String[] args) {
+        int Var1 = 0;
+        int Var2 = 1;
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入数列的项数：");
         int num = sc.nextInt();
-        int[] result = new int[num];
-        result[0] = 1;
-        result[1] = 1;
-        for (int i = 2; i < num; i++) {
-            result[i] = result[i - 1] + result[i - 2];
-        }
-        System.out.println("斐波那契数列的前" + num + "项是：");
-        for (int i = 0; i < num; i++) {
-            System.out.print(result[i] + " ");
+        for (int i = 1; i <= num; i++) {
+            System.out.print(Var1 + " ");
+            int sum = Var1 + Var2;
+            Var1 = Var2;
+            Var2 = sum;
         }
     }
 }
