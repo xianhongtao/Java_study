4002e18322bf44b3a343fcf5422234cf04d9bbb3
xianhongtao <xianhongtao2022@outlook.com>
2025-04-26 22:20:40 +0800

    新增 CodeForce2090A 类，支持处理输入并输出结果

---
+++ CodeForcesSolutions/CodeForce2090A.java
@@ -0,0 +1,31 @@
+package CodeForcesSolutions;
+import java.util.Scanner;
+
+public class CodeForce2090A {
+    public static void main(String[] args)
+    {
+        String[] input;
+        int x, y, a, loopLenth;
+        Scanner sc = new Scanner(System.in);
+        int caseInput = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < caseInput; i++)
+        {
+            input = sc.nextLine().split(" ");
+            x = Integer.parseInt(input[0]);
+            y = Integer.parseInt(input[1]);
+            a = Integer.parseInt(input[2]);
+            loopLenth = x + y;
+            a = a % loopLenth;
+            if (a < x)
+            {
+                System.out.println("NO");
+            }
+            else
+            {
+                System.out.println("YES");
+            }
+        }
+        sc.close();
+    }
+}
